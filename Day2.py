ab6e907d6a1bf1ad4bef7a9779629fa3ac308818
Tarun Abhishek <110079134+TarunAbhishek@users.noreply.github.com>
2025-02-06 00:03:23 +0530

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,43 @@
+#23456789
+#25789999
+
+print(23456789+25789999) #addition
+print(23456789-25789999) #subtract
+print(23456789*25789999) #mutliplication
+print(23456789/25789999) #Division
+print(23456789//25789999) #Exponent
+
+
+a=23456789
+b=25789999
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+
+print("Cheppanu brother")
+bhAAi= "Cheppanu Brother"
+print(bhAAi)
+print(len(bhAAi))
+num1= "     "
+print(len(num1))
+num2=""
+print(num2)
+str1="Party undhi Pushpa"
+print(str1)
+print(str1[2]) #using Indexing concept(positive)
+print(str1[17]) #using Indexing concept(Out of range)
+print(str1[-16]) #using Indexing concept(negative range)
+
+
+#Slicing
+print(str1[0:5]) #Using Slicing concept but 5th element will not be 
+print(str1[0:11]) #Using Slicing concept but upto some extent
+print(str1[0:]) 
+print(str1[:])
+print(str1[3:1000])
+print(str1[-8:-2:2])
+#       index index step
+
+#Task every concept with example!!
